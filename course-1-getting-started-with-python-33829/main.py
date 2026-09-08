5d119321a363e0e5d3d0a02f841365e92c791174
Dvnte <Kebinigbanibo@gmail.com>
2026-09-08 10:03:14 +0100

    Complete: Course 1: Getting Started with Python

---
+++ course-1-getting-started-with-python-33829/main.py
@@ -0,0 +1,12 @@
+user = input("Enter name: ")
+hobbies = input("Enter hobbies: ")
+fav_books = input("Enter your favourite books: ")
+movies = input("Enter your favourite movies: ")
+print(f"Hello, {user}") #Greet user with their inputted name
+print(f"Your hobbies are:, {hobbies}")
+print(f"swimming\nreading\ndancing")
+print(f"Your favourite books are: {fav_books},", end = " ") #
+print("Nova\tCorp", end = " ")
+print("Green\\Lantern")
+print(f"Your favourite movies are: \n{movies}") #start favourite movie on a new line
+print("DC", "Marvel", "Capcom", sep = "/") #seperate words with a forward slash
